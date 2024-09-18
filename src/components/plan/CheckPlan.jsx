77ba41d1cc0dbfc03b4377043d455d4695f0a11f
Ulzii0517 <ulziibadrakh.bb@gmail.com>
2024-09-19 02:15:26 +0800

    completed Plan component

--- src/components/plan/CheckPlan.jsx
+++ src/components/plan/CheckPlan.jsx
@@ -1,14 +1,28 @@
+import { CheckIcon } from "../icons";
+
 export const CheckPlan = () => {
   return (
-    <div>
-    <div className="flex">
-      <CheckIcon />
-      <p>Unlimited Bandwitch</p>
-    </div>
-    <div className="flex">
-      <CheckIcon />
-      <p>Unlimited Bandwitch</p>
-    </div>
+    <div className="flex flex-col justify-center gap-5">
+      <div className="flex gap-5">
+        <CheckIcon />
+        <p>Unlimited Bandwitch</p>
+      </div>
+      <div className="flex gap-5">
+        <CheckIcon />
+        <p>Encrypted Connection</p>
+      </div>
+      <div className="flex gap-5">
+        <CheckIcon />
+        <p>Yes Traffic Logs</p>
+      </div>
+      <div className="flex gap-5">
+        <CheckIcon />
+        <p>Works on All Devices</p>
+      </div>
+      <div className="flex gap-5">
+        <CheckIcon />
+        <p>Connect Anyware</p>
+      </div>
     </div>
   );
 };
--- src/components/plan/Plan.jsx
+++ src/components/plan/Plan.jsx
@@ -1,9 +1,10 @@
 import { CheckIcon } from "../icons";
+import { CheckPlan } from "./CheckPlan";
 
 export const Plan = () => {
   return (
-    <div className="flex justify-center w-full bg-gray-100">
-      <div className="container max-w-[1250px] w-full">
+    <div className="flex justify-center w-full bg-gray-50">
+      <div className="container max-w-[1250px] w-full py-[60px] box-border ">
         <div className="flex flex-col items-center gap-3">
           <div className="text-[35px] font-semibold leading-[50px]">
             Choose Your Plan
@@ -13,32 +14,70 @@ export const Plan = () => {
             and cheerfully.
           </div>
         </div>
-        <div className="flex ">
+        <div className="flex justify-between pt-[50px]">
           <div className="flex flex-col gap-5 items-center max-w-[330px] w-full h-[760px] border border-gray-300 bg-white rounded-lg">
             <div className="pt-[50px] w-full flex justify-center max-w-[150px] ">
               <img src="./box.png" alt="" />
             </div>
-            <p className="text-[18px] font-semibold leading-8 text-center">Free Plan</p>
+            <p className="text-[18px] font-bold leading-8 text-center">
+              Free Plan
+            </p>
             <div className="flex flex-col justify-center gap-5">
+              <div className="flex gap-5">
+                <CheckIcon />
+                <p>Unlimited Bandwitch</p>
+              </div>
+              <div className="flex gap-5">
+                <CheckIcon />
+                <p>Encrypted Connection</p>
+              </div>
+              <div className="flex gap-5">
+                <CheckIcon />
+                <p>No Traffic Logs</p>
+              </div>
+              <div className="flex gap-5">
+                <CheckIcon />
+                <p>Works on All Devices</p>
+              </div>
+            </div>
+
+            <p className="flex justify-end text-[25px] font-medium leading-[30px] text-center pt-32">
+              Free
+            </p>
+            <div className=" max-w-[180px] w-full btn btn-outline btn-error rounded-[30px]">
+              Select
+            </div>
+          </div>
+          <div className="flex flex-col gap-5 items-center max-w-[330px] w-full h-[760px] border border-gray-300 bg-white rounded-lg">
+            <div className="pt-[50px] w-full flex justify-center max-w-[150px] ">
+              <img src="./box.png" alt="" />
+            </div>
+            <p className="text-[18px] font-bold leading-8 text-center">
+              Standart Plan
+            </p>
+            <CheckPlan />
+
+            <p className="flex justify-end text-[25px] font-medium leading-[30px] text-center pt-[83px]">
+              Free
+            </p>
+            <div className=" max-w-[180px] w-full btn btn-outline btn-error rounded-[30px]">
+              Select
+            </div>
+          </div>
+          <div className="flex flex-col gap-5 items-center max-w-[330px] w-full h-[760px] border border-gray-300 bg-white rounded-lg">
+            <div className="pt-[50px] w-full flex justify-center max-w-[150px] ">
+              <img src="./box.png" alt="" />
+            </div>
+            <p className="text-[18px] font-bold leading-8 text-center">
+              Premium Plan
+            </p>
+            <CheckPlan />
             <div className="flex gap-5">
-              <CheckIcon />
-              <p>Unlimited Bandwitch</p>
-            </div>
-            <div className="flex gap-5">
-              <CheckIcon />
-              <p>Encrypted Connection</p>
-            </div>
-            <div className="flex gap-5">
-              <CheckIcon />
-              <p>No Traffic Logs</p>
-            </div>
-            <div className="flex gap-5">
-              <CheckIcon />
-              <p>Works on All Devices</p>
-            </div>
-            </div>
-            
-            <p className="flex justify-end text-[25px] font-medium leading-[30px] text-center ">                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+                <CheckIcon />
+                <p>Get New Features</p>
+              </div>
+
+            <p className="flex justify-end text-[25px] font-medium leading-[30px] text-center pt-[40px]">
               Free
             </p>
             <div className=" max-w-[180px] w-full btn btn-outline btn-error rounded-[30px]">
